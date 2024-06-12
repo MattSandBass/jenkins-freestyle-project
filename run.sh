21f1e21aee92db6d8e9a1e2b077e31024eebf3cf
Matt Sandford <49644676+MattSandBass@users.noreply.github.com>
2024-06-12 15:46:42 +0100

    Update run.sh, with new text

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Hello world!' | tee jenkins.txt
+echo 'Hello world! I was triggered with a web hook!' | tee jenkins.txt
 
 echo "Hello from the Jenkins job named: ${JOB_NAME}"
 touch 1.txt 2.txt 3.txt 4.txt 5.txt
